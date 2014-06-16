48e537134ba47c7ec60da49bffcdd3b9ef7d35db
divac lin <divac.d.zoro@gmail.com>
2014-06-16 20:24:33 +0800

    index finish fb api

---
+++ apply/fb.js
@@ -0,0 +1,97 @@
+var userid;
+var ACCESS_TOKEN;
+
+window.fbAsyncInit = function () {//facebook init
+    
+//輸入基本的Facebook init的狀態，與Facebook 連接，包括APP ID的設定
+    FB.init({
+        appId : '322780024547098',                    // App ID from the app dashboard  // wp2014s_final_project:322780024547098  // Lab:699947756683654
+        cookie  : true,                                 // Allowed server-side to fetch fb auth cookie
+        status  : true,                                 // Check Facebook Login status
+        xfbml   : true,                                 // Look for social plugins on the page
+        oauth   : true,
+        version: 'v2.0'
+    });
+
+
+    // define the events when login status changed.
+    FB.Event.subscribe('auth.login', function(response) {
+
+        if (response.status=="connected"){ // if logged in
+            var IfLoggedInDiv=document.getElementById("if-logged-in");
+            IfLoggedInDiv.style.display="inline-block";
+            var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
+            IfNotLoggedInDiv.style.display="none";
+
+            FB.api('/me', function(response) {
+                //    console.log('Good to see you, ' + response.name + '.');
+                    var UserName = document.getElementById("user_name");
+                    UserName.innerHTML=response.name;
+            });
+
+            // get access token
+            ACCESS_TOKEN=response.authResponse.accessToken;
+            //console.log('ACCESS_TOKEN: '+ACCESS_TOKEN);
+
+            // get user id
+            userid=response.authResponse.userID;
+            console.log('userid: '+userid);
+        }
+    });
+            
+    FB.Event.subscribe('auth.logout', function(response) {
+        if (response.status!="connected"){ // if logged out
+            var IfLoggedInDiv=document.getElementById("if-logged-in");
+            IfLoggedInDiv.style.display="none";
+            var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
+            IfNotLoggedInDiv.style.display="inline-block";
+            FB.logout(function(response){
+                location.reload();  // refresh
+            });
+        }
+    });
+
+    $("#login-btn").click(function(){   
+        //alert("click on login-btn"); 
+        FB.login(function(response) {
+            //console.log(response);
+            if (response.status == "connected") {
+                var IfLoggedInDiv=document.getElementById("if-logged-in");
+                IfLoggedInDiv.style.display="inline-block";
+                var IfNotLoggedInDiv=document.getElementById("if-not-logged-in");
+                IfNotLoggedInDiv.style.display="none";
+                
+                FB.api('/me', function(response) {
+                //    console.log('Good to see you, ' + response.name + '.');
+                    var UserName = document.getElementById("user_name");
+                    UserName.innerHTML=response.name;
+                });
+                
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        }, {scope:'user_birthday,user_friends,user_photos,user_status,friends_status,friends_checkins,friends_photos,read_stream,export_stream'}); 
+    });
+
+    $("#logout-btn").click(function(){
+        //alert('You are logging out. Bye!');
+        FB.logout(function(response){
+            location.reload();  // refresh
+        });
+    });
+
+}; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<init end
+
+
+//LOAD FACEBOOK SDK ASYNC，這是基本的東西，應該不用多說了吧
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js"; 
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+  
--- index.js
+++ index.js
@@ -5,11 +5,13 @@ $().ready(function(){
 	$("article").append(html);
 	var ul_fixed=(window.screen.width-354)/2-200;
 	var search_fixed=(window.screen.width-220)/2+ul_fixed-180;
-	$("article section").append('<ul id="index_ul" style="left:'+ul_fixed+'px;"><img src="img/listyle.png"><li><a>關於我們</a></li><img src="img/listyle.png"><li><a>查看連署</a></li><img src="img/listyle.png"><li><a>登入/註冊</a></li><img src="img/listyle.png"><li><a>搜尋連署</a></li></ul>');
+	$("article section").append('<ul id="index_ul" style="left:'+ul_fixed+'px;"><img src="img/listyle.png"><li><a>關於我們</a></li><img src="img/listyle.png"><li><a href="catagory.html">查看連署</a></li><img src="img/listyle.png">	<li id="if-not-logged-in"><a href="#" id="login-btn">登入/註冊</a></li><li id="if-logged-in" style="display:none"><a href="#" id="logout-btn">登出</a></li></li><img src="img/listyle.png"><li><a>搜尋連署</a></li></ul>');
 	$("article section").append('<div class="input-group col-lg-3" style="width:220px; position:relative; left:'+search_fixed+'px; bottom:-120px;" ><input type="text" class="form-control"><span class="input-group-btn"><button class="btn btn-custom" type="button"><span class="glyphicon glyphicon-search white"></span></button></span></div>');	
 	var index02_height_pos_fixed=(window.screen.height-(window.screen.width-400)*947/1857)/2-100;
-	html='<section style="width:100%; height:'+height+'px; background:#232A34;margin:20px 0 0 0; text-align:center;"><img style="width:'+(window.screen.width-400)+'px; padding-top:'+index02_height_pos_fixed+'px;" src="img/index02.png"/></section>';
+	html='<section style="width:100%; height:'+height+'px; background:#232A34;margin-top:20px; text-align:center;"><img style="width:'+(window.screen.width-400)+'px; padding-top:'+index02_height_pos_fixed+'px;" src="img/index02.png"/></section>';
 	$("article").append(html);
-	
-	$("article").append(html);	
+	var index03_height_pos_fixed=window.screen.height/5;
+	html='<section id="index03_layer_1" style="width:100%; height:'+window.screen.height+'px; text-align:center; background:#232A34; margin-top:20px;"><img id="index_want_graph" src="img/want_graph.png" style="height:'+index03_height_pos_fixed+'px; margin-top:'+index03_height_pos_fixed*4/5+'px; margin-right:'+index03_height_pos_fixed*2/5+'px;"/><img id="index_establish_graph" src="img/establish_graph.png" style="height:'+index03_height_pos_fixed+'px; margin-top:'+index03_height_pos_fixed*4/5+'px; margin-left:'+index03_height_pos_fixed*2/5+'px;"/><br/></section>';
+	$("article").append(html); 
+	$("#index03_layer_1").append('<a href="catagory.html"><img src="img/want_text.png" style="width:193px; margin-top:40px; margin-right:'+index03_height_pos_fixed*2/5+'px;"/></a><a href="#"><img src="img/establish_text.png" style="width:193px; margin-top:40px; margin-left:'+index03_height_pos_fixed*2/5+'px;"/></a>');	
 });
