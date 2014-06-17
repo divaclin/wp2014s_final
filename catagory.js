2aec7cc0dda125aaaf3bda3c1fc6b5a4a2c0cc6b
divac lin <divac.d.zoro@gmail.com>
2014-06-18 02:09:32 +0800

    sure

--- catagory.js
+++ catagory.js
@@ -18,9 +18,7 @@ function CatagoryChange(e){
 	  success: function(data) {
 		  for(var i=0;i<data.length;i++){
 			  console.log(data[i]);
-			  var translation=data[i].toJSON();
-			  console.log(translation);
-              $("#catagory_content").append('<div class="issue-block"><div class="issue-content"><p>'+data[i].toJSON().title+'</p><img src="'+translation.picture+'" class="issue-img" /><div>'+translation.creatAt+'</div><span><a href="#">我要連署</a><a href="#">連署聲明</a></span></div></div>');  
+              $("#catagory_content").append('<div class="issue-block"><div class="issue-content"><p>'+data[i].toJSON().title+'</p><img src="'+data[i].toJSON().picture+'" class="issue-img" /><div>'+data[i].toJSON().creatAt+'</div><span><a href="#">我要連署</a><a href="#">連署聲明</a></span></div></div>');  
 		  }
 	  }
 	});
