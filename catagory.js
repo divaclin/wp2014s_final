df617a4a4021d697e829268461f642d1aaa72f28
divac lin <divac.d.zoro@gmail.com>
2014-06-18 00:09:43 +0800

    category miss spell QAQ

--- catagory.js
+++ catagory.js
@@ -1,11 +1,21 @@
 var catagorylist=["img/earth.png","img/p_and_e.png","img/e_and_c.png","img/life.png","img/labor.png"];
+var catagory=["environment-and-land","politics-and-economics","education-and-culture","livelihood","labor"];
 
 $().ready(function(){
 	var catagory_photo_width=window.screen.width*3/5;
     $("article").append('<section style="background:#fff; margin-top:77px; text-align:center; height:'+window.screen.height*2/3+'px;"><img id="catagory_photo" src="'+catagorylist[0]+'" style="width:'+catagory_photo_width+'px;"/></section>');
-	$("article").append('<section style="background:#232A34; height:'+2*window.screen.height+'px; margin-top:20px"></section>');
+	$("article").append('<section id="catagory_content" style="background:#232A34; height:'+2*window.screen.height+'px; margin-top:20px"></section>');
 });
 
 function CatagoryChange(e){
+	$("#catagory_content").empty();
 	$("#catagory_photo").attr("src", catagorylist[e]);
+	var query = new Parse.Query(Parse.Petition);
+	query.equalTo(category,catagory[e]);
+	query.find({
+	  success: function(content) {
+		  console.log(content);
+			 
+	  }
+	});
 }
