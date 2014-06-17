793d940e7af436f8499d46b7b92a6e1def7a0497
divac lin <divac.d.zoro@gmail.com>
2014-06-18 00:15:35 +0800

    fixed

--- catagory.js
+++ catagory.js
@@ -5,12 +5,13 @@ $().ready(function(){
 	var catagory_photo_width=window.screen.width*3/5;
     $("article").append('<section style="background:#fff; margin-top:77px; text-align:center; height:'+window.screen.height*2/3+'px;"><img id="catagory_photo" src="'+catagorylist[0]+'" style="width:'+catagory_photo_width+'px;"/></section>');
 	$("article").append('<section id="catagory_content" style="background:#232A34; height:'+2*window.screen.height+'px; margin-top:20px"></section>');
+	
 });
 
 function CatagoryChange(e){
 	$("#catagory_content").empty();
 	$("#catagory_photo").attr("src", catagorylist[e]);
-	var query = new Parse.Query(Parse.Petition);
+	var query = new Parse.Query(Petition);
 	query.equalTo(category,catagory[e]);
 	query.find({
 	  success: function(content) {
