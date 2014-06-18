00ba377829857977ee5cc546e8aa90e870dec93c
divac lin <divac.d.zoro@gmail.com>
2014-06-18 13:55:57 +0800

    category fixed

--- catagory.js
+++ catagory.js
@@ -9,7 +9,11 @@ $().ready(function(){
 });
 
 function CatagoryChange(e){
-	$("#catagory_content").empty();
+	
+	$("article").empty();
+	var catagory_photo_width=window.screen.width*3/5;
+    $("article").append('<section style="background:#fff; margin-top:77px; text-align:center; height:'+window.screen.height*2/3+'px;"><img id="catagory_photo" src="'+catagorylist[0]+'" style="width:'+catagory_photo_width+'px;"/></section>');
+	$("article").append('<section id="catagory_content" style="background:#232A34; height:'+2*window.screen.height+'px; margin-top:20px"></section>');
 	$("#catagory_photo").attr("src", catagorylist[e]);
 	var Petition = Parse.Object.extend('Petition');
 	var query = new Parse.Query(Petition);
